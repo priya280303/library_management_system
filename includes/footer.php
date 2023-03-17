565f86747c801b3ec180efb128b430501929067c
Krishna Priya.N <85284518+priya280303@users.noreply.github.com>
2023-03-17 23:18:58 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Open Admin Panel and Add/View user to know Login ID</b>
+      </div>
+
+      
+      <strong>Library Management System | Designed by: 🧑‍🎓</strong>
+      </div>
+</footer>
